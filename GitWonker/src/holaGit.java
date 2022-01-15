3e133f0f3d3f3f2a9433613fe2d9d364825d9a3a
OtterBlue <96080748+OtterBlue@users.noreply.github.com>
2022-01-15 16:53:47 +0100

    Agregados saludos

--- GitWonker/src/holaGit.java
+++ GitWonker/src/holaGit.java
@@ -1,9 +1,15 @@
+import java.util.*;
 
 public class holaGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Hola Git");
+		System.out.println("Hola humano ¿Cómo estás?");
 
+		
 	}
 
 }
