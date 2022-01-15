137b69040015ffb7f6a2936d60bb719ac9785a4e
OtterBlue <96080748+OtterBlue@users.noreply.github.com>
2022-01-15 16:59:59 +0100

    Agregada pregunta

--- GitWonker/src/holaGit.java
+++ GitWonker/src/holaGit.java
@@ -7,7 +7,10 @@ public class holaGit {
 		Scanner sc=new Scanner(System.in);
 		
 		System.out.println("Hola Git");
-		System.out.println("Hola humano ¿Cómo estás?");
+		System.out.println("Hola humano");
+		System.out.println("¿Como estás?");
+		String estado=sc.nextLine();
+		
 
 		
 	}
